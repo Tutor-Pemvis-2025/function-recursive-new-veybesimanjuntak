40b4b36d7622f51628bf069b899dc3151973e46a
Veybe Youlanda K M Simanjuntak <208570976+veybesimanjuntak@users.noreply.github.com>
2025-11-11 21:11:11 +0700

    NIM 24, 14, 33

--- TFUNC.java
+++ TFUNC.java
@@ -9,24 +9,26 @@ public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n, hasil;
+        int x, hasil;
 
-        n = Integer.parseInt(input.nextLine());
-        hasil = pertambahan(n);
-        System.out.println("Bilangan yang dimasukkan : " + n);
-        System.out.println("Hasil perhitungan rekursif : " + hasil);
+        x = Integer.parseInt(input.nextLine());
+        System.out.println("Bilangan yang dimasukkan : " + x);
+        angka(x);
+        System.out.println("Hasil perhitungan rekursif : " + angka(x));
     }
     
-    public static int pertambahan(int n) {
-        int hasil;
+    public static int angka(int x) {
+        int result, hasilb;
 
-        if (n == 1) {
-            hasil = 1;
+        if (x == 1) {
+            result = 1;
         } else {
-            hasil = pertambahan(n - 1) + n;
+            hasilb = angka(x - 1);
+            result = x + hasilb;
         }
         
-        return hasil;
+        return result;
     }
 }
+
 
